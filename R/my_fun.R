327b12eeffc61361f0d14bdf64e031e0c7e0c0e3
AnnaEGardell <annagar@kth.se>
2025-06-13 11:18:07 +0200

    Now I have added some roxygen stuff

--- R/my_fun.R
+++ R/my_fun.R
@@ -1,4 +1,16 @@
 
+#' Summarise installed R packages by library
+#'
+#'This function counts the number of packages installed
+#'in each R library in your current environment.
+#'
+#' @returns A df with one row per library path, and a column for its package count
+#' @export
+#'
+#' @examples
+#' # Get a summary of installed packages by library path
+#' lib_summary()
+#'
 lib_summary <- function() {
   pkgs <- utils::installed.packages()
   pkg_tbl <- table(pkgs[, "LibPath"])
