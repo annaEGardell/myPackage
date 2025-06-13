76dda4aaa9d5be68baefd7063544553d53f91297
AnnaEGardell <annagar@kth.se>
2025-06-13 11:47:27 +0200

    Added a readme and some other things

---
+++ R/myPackage-package.R
@@ -0,0 +1,7 @@
+#' myPackage: Summarise Installed R Packages by Library
+#'
+#' This package provides a single function, \code{lib_summary()}, that returns the number of installed packages in each R library path used by your environment.
+#'
+#' "_PACKAGE"
+#' @name myPackage
+NULL
